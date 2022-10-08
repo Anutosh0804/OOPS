f567d760f654b96694558eb67f1204c0183e4f94
Fraud Vestige <73070611+fraudVestige@users.noreply.github.com>
2022-10-08 16:41:27 +0530

    Commit 2 FV

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -19,4 +19,20 @@ int main(){
         
         cout<<ans<<endl;
     }
+    for(int i=1;i<=k;i++){
+        string ans="";
+        if(i%3==0)
+            ans+="Fizz";
+        if(i%5==0)
+            ans+="Buzz";
+        if(i%7==0)
+            ans+="Fuzz";
+        if(i%11==0)
+            ans+="Anto"1<<endl;
+        else
+            if(ans=="")
+                ans=to_string(i);
+        
+        cout<<ans<<endl;
+    
 }
