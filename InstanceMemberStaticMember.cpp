15bbdf368bc3392c7d5f68adbd6840c57c7d4edc
Purujit <41872519+GuptaPurujit@users.noreply.github.com>
2022-10-18 16:45:03 +0530

    Update InstanceMemberStaticMember.cpp

--- InstanceMemberStaticMember.cpp
+++ InstanceMemberStaticMember.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Complex{  //a+ib
+class complex{  //a+ib
     int a;  //Instance member variable
     int b;  // ''
     static float root2;  //static member variable, class member variable, decleared in class 
@@ -20,7 +20,7 @@ public:
     }
 };
 
-float Complex::root2=1.414f;    //class member defined outside class, does not belong to an object but to whole class
+float complex::root2=1.414f;    //class member defined outside class, does not belong to an object but to whole class
 //only one copy exist for whole lifetime, any obj can use same copy of class variable,
 //can also be used with class name (only if variable are public)
 
@@ -28,5 +28,5 @@ float Complex::root2=1.414f;    //class member defined outside class, does not b
 int main(){
    //cout<<Complex::root2<<endl;  //cannot use with class name directly because it is private
    //can access with without obj and without public using static member function(only can acccess static memb var)
-   cout<<Complex::showroot()<<endl;
+   cout<<complex::showroot()<<endl;
 }
