8c50c3b1cc9a9fa501f81289223454f902c69770
Nikhil Raj <119805180+Nikhil-IITG@users.noreply.github.com>
2023-10-15 18:27:24 +0530

    BST_completelyImplemented.cpp

--- BST_completelyImplemented.cpp
+++ BST_completelyImplemented.cpp
@@ -54,17 +54,34 @@ void inorderBST(node* root){
 
 node* deleteBST(node* root,int ele){
     node* ntd=searchBST(root,ele);
-    if(ntd==root && ntd->right==NULL && ntd->left==NULL){
+    if(ntd==root && ntd->right==NULL && ntd->left==NULL){  //If there's only 1 node in the tree
         free(ntd);
-    }else if(ntd->left==NULL){
+    }
+    else if(ntd==root && ntd->right==NULL){       //root has elements on only left subtree and to delete the root itself
+        ntd->data=ntd->left->data;
+        node* nsu=ntd->left;
+        ntd->left=nsu->left;
+        ntd->right=nsu->right;
+        free(nsu);
+    }
+    else if(ntd==root && ntd->left==NULL){        //root has elements on only right subtree and to delete the root itself
+        ntd->data=ntd->right->data;
+        node* nsu=ntd->right;
+        ntd->left=nsu->left;
+        ntd->right=nsu->right;
+        free(nsu);
+    }
+    else if(ntd->left==NULL){                   //when the left subtree of the node to delete is NULL
         if(ntd->parent->right==ntd){ntd->parent->right=ntd->right;}
         else{ntd->parent->left=ntd->right;}
         free(ntd);
-    }else if(ntd->right==NULL){
+    }
+    else if(ntd->right==NULL){                   //when the right subtree of the node to delete is NULL
         if(ntd->parent->right==ntd){ntd->parent->right=ntd->left;}
         else{ntd->parent->left=ntd->left;}
         free(ntd);
-    }else{
+    }
+    else{                                         //for all other cases
         node* part=inorderSucc(ntd->right);
         ntd->data=part->data;
         if(part->parent->left->data==part->data){
@@ -75,33 +92,31 @@ node* deleteBST(node* root,int ele){
         }  
         free(part);
 
-    }
+    }return root;
 }
 
 int main(){
     node* root=NULL;
-    root=insertBST(root,NULL,15);root->parent=NULL;
-    insertBST(root,NULL,3);
-    insertBST(root,NULL,18);
-    insertBST(root,NULL,5);
-    insertBST(root,NULL,4);
-    insertBST(root,NULL,14);
-    insertBST(root,NULL,7);
-    insertBST(root,NULL,9);
-    insertBST(root,NULL,8);
-    insertBST(root,NULL,11);
+    
+    int f;cout<<"Enter the number of nodes you want to add in the BST: ";cin>>f;
+    cout<<"Enter "<<f<<" space separated integers: ";
+    for(int i=0;i<f;i++){
+        int a;cin>>a;
+        if(i==0){root=insertBST(root,NULL,a);root->parent=NULL;}
+        else{insertBST(root,NULL,a);}
+    }
     inorderBST(root);
     cout<<'\n';
     int key;cout<<"Enter key to find in the BST: ";cin>>key;
     if(searchBST(root,key)==NULL){cout<<key<<" not found\n";}
     else{cout<<key<<" found\n";}
-    int k;cout<<"Enter number to search: ";cin>>k;
+    int k;cout<<"Enter number to search for it's parent and successor: ";cin>>k;
     node* fsucc=searchBST(root,k);
     node* a= inorderSucc(fsucc->right);
     if(fsucc!=root){cout<<"Parent: "<<fsucc->parent->data<<"\n";}
     else{cout<<"Parent: NULL\n";}
-    if(fsucc->right!=NULL){cout<<"Succ: "<<a->data<<"\n";}
-    else{cout<<"Succ: NULL\n";}
+    if(fsucc->right!=NULL){cout<<"Successor: "<<a->data<<"\n";}
+    else{cout<<"Successor: NULL\n";}
     int y;cout<<"Enter element to delete: ";cin>>y;
     deleteBST(root,y);
     inorderBST(root);
